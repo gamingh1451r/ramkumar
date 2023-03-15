20e3a4933c89d02a7cce4e6af8e7c6cc53f56142
gamingh1451r <127920179+gamingh1451r@users.noreply.github.com>
2023-03-15 14:19:29 +0530

    Create ram.c

---
+++ ram.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d",&a,&b);
+    printf("\n%d %d",b,a);
+    printf("\n %d",(a+b));
+    printf("\n %d",(a%b));
+    return 0;
+}
+
