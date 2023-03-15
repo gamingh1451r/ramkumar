34f5a073d9b6fadf249589da286bfcab70876923
gamingh1451r <127920179+gamingh1451r@users.noreply.github.com>
2023-03-15 16:09:28 +0530

    Create ram4.c

---
+++ ram4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    if(a%2||a%3)
+    {
+        printf("number divisible by 2 or 3");
+    }
+      else 
+      {
+          printf("no number divisible by 2 or 3");
+      }
+}
